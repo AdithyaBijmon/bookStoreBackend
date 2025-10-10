0973c79e59a7412d208e320a60ee84389c15dc2e
AdithyaBijimon <adithyabijimon5@gmail.com>
2025-10-10 11:06:54 +0530

    Server initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+exports.registerController = (req, res) => {
+
+    console.log('Inside register api')
+    //   console.log(req.body)
+    const { username, email, password } = req.body
+    console.log(username, email, password)
+
+    res.status(200).send("Register request received")
+
+}
---
+++ index.js
@@ -0,0 +1,35 @@
+// import express cors dotenv
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes/router')
+
+// loads .env file contents to process.env
+require('dotenv').config()
+
+// create server
+const bookStoreServer = express()
+
+// able cors protocol in server
+bookStoreServer.use(cors())
+
+// parsing
+bookStoreServer.use(express.json())
+
+// routing
+bookStoreServer.use(router)
+
+// create PORT
+const PORT = 5500
+
+
+// resolving http request
+bookStoreServer.get('/',(req,res)=>{
+    res.status(200).send('<h1>Book store Server here...</h1>')
+})
+
+
+
+// run server port
+bookStoreServer.listen(PORT,()=>{
+    console.log(`Book store server started at ${PORT}`) 
+})
