eaf1151263f53643614bd082d477d474acfa0dcc
AdithyaBijimon <adithyabijimon5@gmail.com>
2025-10-11 11:49:26 +0530

    userModel,connection,userController updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,32 @@
-exports.registerController = (req, res) => {
+const users = require("../models/userModel")
+
+exports.registerController = async (req, res) => {
 
     console.log('Inside register api')
     //   console.log(req.body)
     const { username, email, password } = req.body
-    console.log(username, email, password)
+    // console.log(username, email, password)
+   try{
+    const existingUser = await users.findOne({email})
 
-    res.status(200).send("Register request received")
+    if(existingUser){
+        res.status(409).json("User already exists. Please Login")
+    }
+    else{
+
+        const newUser = new users({
+            username,
+            email,
+            password
+        })
+
+        await newUser.save()
+        res.status(200).json(newUser)
+    }
+   }
+   catch(err){
+     res.status(500).json(err)
+   }
+    
 
 }
